ab6682de86eb15f4aac0d4a079c390a534da4357
Mathieu Chatillon <chatillon.mathieu@gmail.com>
2019-12-28 00:10:26 +0100

    Add API fetch on Explore component

--- src/containers/Explore/Explore.js
+++ src/containers/Explore/Explore.js
@@ -1,34 +1,40 @@
-import React, { useState } from 'react';
-import { sampleSize, find } from 'lodash';
+import React, { useState, useEffect } from 'react';
 
 import { Results } from './Explore.styles';
 
 import Card from 'components/Card/Card';
 import Instruments from 'components/Instruments/Instruments';
 
-import { users, profiles } from 'data';
+import axios from 'axios';
 
 const Explore = () => {
-  const [results] = useState(sampleSize(users, 10));
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    async function fetchResults() {
+      axios.get('//localhost:3004/users?_start=0&_end=10')
+        .then(response => setResults(response.data))
+        .catch(error => console.log(error))
+    }
+    fetchResults();
+  }, []);
 
   return(
     <>
       <h1>Explore</h1>
       <Results>
         {results.map(result => {
-          // get the user profile
-          const profile = find(profiles, {user_id: result.id});
 
           // display the Card
           return(
             <Card
               key={result.id}
-              image={profile.picture}
-              title={profile.display_name}
+              image={result.profile.picture}
+              title={result.profile.display_name}
               url={`/profile/${result.username}`}
-              urlLabel={`Go to ${profile.display_name}'s profile.`}
-              text={profile.location}
-              slot={<Instruments items={profile.instruments} />}
+              urlLabel={`Go to ${result.profile.display_name}'s profile.`}
+              text={result.profile.location}
+              slot={<Instruments items={result.profile.instruments} />}
               rounded
             />
           )
