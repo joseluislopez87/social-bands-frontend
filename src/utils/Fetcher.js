9b2119c6b595bac746790392589c810ee6bf33bf
Mathieu Chatillon <chatillon.mathieu@gmail.com>
2019-12-30 22:08:54 +0100

    Remove empty data handler from Fetcher component
    The Fetcher component shouldn't handle empty data, this should be the component's responsibility, move that logic into each component's render method

--- src/utils/Fetcher.js
+++ src/utils/Fetcher.js
@@ -5,23 +5,14 @@ import axios from 'axios';
 import Loading from 'components/Loading';
 import Error from './Error';
 
-const defaultEmptyText = 'Nothing to show.';
-
-const defaultOptions = {
-  customLoading: undefined,
-}
-
 export default function Fetcher({
   request,
   children,
-  emptyText = defaultEmptyText,
-  options = defaultOptions
+  customLoading
 }) {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
-
-  const { customLoading } = options;
 
   useEffect(() => {
     async function fetchData() {
@@ -29,19 +20,18 @@ export default function Fetcher({
       await axios(request)
         .then(data => setData(data.data))
         .catch(error => setError(error));
-      setLoading(false);
+      setTimeout(() => setLoading(false), 0);
     }
     fetchData();
   }, [request]);
 
   if (loading) return customLoading ? customLoading : <Loading />
   if (error) return <Error error={error} />
-  if (data.length === 0) return emptyText
   return children({ data: data })
 }
 
 Fetcher.propTypes = {
   request: PropTypes.string.isRequired,
   children: PropTypes.func.isRequired,
-  options: PropTypes.object,
+  customLoading: PropTypes.node,
 }
